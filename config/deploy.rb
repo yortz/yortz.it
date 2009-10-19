82eb1f5ad806b48c29f9a83d34aa5dad887c3b1c
yortz <skylarking420@gmail.com>
2009-10-19 19:36:54 +0200

    modified rake task to remove rakefile and config dir from _site, removed after callback from deploy.rb to not config_vhost each time after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,13 @@ set :user, 'yortz'
 set :runner, 'yortz'
 set :keep_releases, 1 
 
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/jekyll/#{application}"
+set :current_deploy_dir, "#{deploy_to}/current"
+set :deploy_via, :remote_cache
+set :tmp_dir, "#{deploy_to}/tmp"
 
+# set :deploy_to, "/var/www/#{application}"
 set :branch, "master"
-
 
 ssh_options[:paranoid] = false
 default_run_options[:pty] = true
@@ -55,3 +58,51 @@ end
  
 after "deploy:setup", "deploy:set_app_dir_access" 
 after "deploy:update_code", "jekyll:generate_site"
+after "deploy:symlink", "apache:ls_app"
+# after "apache:ls_app" , "apache:config_vhost"
+
+namespace :apache do
+  
+  desc "link to www"
+  task :ls_app do
+  run "cd /var/www"
+  sudo "ln -s #{deploy_to}/current/_site /var/www/#{application}"
+  end
+
+  desc "Configure VHost"
+  task :config_vhost do
+    vhost_config =<<-EOF
+     <VirtualHost *:80>
+      ServerName #{application}
+      DocumentRoot #{deploy_to}/current/_site
+      CustomLog #{shared_path}/log/access.log common
+      ErrorLog #{shared_path}/log/error.log
+      <Directory "#{deploy_to}/current/_site">
+      Options FollowSymLinks
+      AllowOverride None
+      Order allow,deny
+      Allow from all 
+      </Directory>
+      </VirtualHost>
+    EOF
+    put vhost_config, "vhost_config"
+    sudo "mv vhost_config /etc/apache2/sites-available/#{application}"
+    sudo "a2ensite #{application}"
+  end
+  
+  desc "Enable Site"
+  task :enable_vhost do
+  sudo "a2ensite #{application}"
+end
+
+desc "Reload Apache"
+task :reload do
+  sudo "/etc/init.d/apache2 reload"
+end
+
+desc "Disable Site"
+task :disable_vhost do
+sudo "a2dissite #{custom_server_address}"
+end
+
+end
